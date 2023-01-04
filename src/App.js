ddfe10d782169d1a9491105215613d44fcf42901
Sakshi <ingolesakshi1@gmail.com>
2023-01-03 19:50:32 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 
-<<<<<<< HEAD
 
 import { Route, Switch } from 'react-router-dom';
 import Header from './component/Header';
-=======
-import { Route, Switch } from 'react-router-dom';
-import Header from './component/Header';
-import Home from './component/Home';
->>>>>>> 7d927940ffd050d0516548a40263150b38699d00
 import About from './component/About';
 import Home from './component/Home';
 import Expertise from './component/Expertise';
@@ -15,7 +9,6 @@ import Expertise from './component/Expertise';
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Header />
       
       <Switch>
@@ -23,16 +16,9 @@ function App() {
         <Route path="/expertise" exact component={Expertise}></Route>
 
         
-      </Switch> */}
+      </Switch> 
 
 
-=======
-    <Header/>
-    <Switch>
-      <Route path="/about" exact component={About} />    
-      <Route path="/" exact component={Home} />    
-    </Switch>
->>>>>>> 7d927940ffd050d0516548a40263150b38699d00
     </>
   );
 }
